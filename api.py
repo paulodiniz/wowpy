f481702d5bfa2adef5cd94e0512ebd2be195c93d
paulodiniz <paulo.rc.diniz@gmail.com>
2013-01-30 22:28:22 -0200

    teste

--- api.py
+++ api.py
@@ -2,6 +2,7 @@ import requests
 import jsonpickle
 from constants import * 
 from apis.character import Character
+from apis.race import Race
 
 class API(object):
 
@@ -9,15 +10,25 @@ class API(object):
 		self.realm = realm
 		self.locale = locale	
 
+	def retrieve_race(self,race_id):
+		r = requests.get(BATTLE_NET_URL + "/data/character/races")
+		decoded = jsonpickle.decode(r.text)
+		for race in decoded['races']:
+			if race['id'] == race_id:
+				r = Race(race_id = race['id'], mask = race['mask'], side = race['side'], name = race['name'])
+				return r
+		
 	def retrieve_char(self, name):
 		r = requests.get(BATTLE_NET_URL + "/character/" + self.realm + "/" + name)
 		decoded = jsonpickle.decode(r.text)
-		character = Character(name = decoded['name'], realm = decoded['realm'], battlegroup = decoded['battlegroup'], char_class = decoded['class'], race = decoded['race'], gender = decoded['gender'], level = decoded['level'], achievementPoints = decoded['achievementPoints'])
+		race = self.retrieve_race(decoded['race'])
+		character = Character(name = decoded['name'], realm = decoded['realm'], battlegroup = decoded['battlegroup'], char_class = decoded['class'], race = race, gender = decoded['gender'], level = decoded['level'], achievementPoints = decoded['achievementPoints'])
 		return character
+	
 
 api = API("Goldrinn")
 charac = api.retrieve_char("Pdiniz")
-print charac.name, charac.race
+print charac.items
 
 
 	
---
+++ apis/race.py
@@ -0,0 +1,7 @@
+class Race(object):
+	def __init__(self, race_id, mask, side, name):
+		self.race_id = race_id
+		self.mask = mask
+		self.side = side
+		self.name = name
+		
